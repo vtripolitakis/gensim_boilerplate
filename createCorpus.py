14fe8c39e1352c5925447b7f4f9f95a8daa631ea
vaggelis <vaggelis@euangelos-tripolitakess-imac.local>
2013-07-02 16:13:55 +0300

    minor change

--- createCorpus.py
+++ createCorpus.py
@@ -5,7 +5,7 @@ from gensim import corpora, models, similarities
 class MyCorpus(object):
     def __iter__(self):
     	di = corpora.Dictionary.load('./mydictionary.dict')
-        for line in open('koko.txt'):
+        for line in open('inputCorpus.txt'):
             # assume there's one document per line, tokens separated by whitespace
             yield di.doc2bow(line.lower().split())
 
